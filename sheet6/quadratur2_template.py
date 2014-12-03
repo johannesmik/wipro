a0f44ade015c588b4c2b23bab53309525c2b81e6
Johannes Mikulasch <mikulasc@in.tum.de>
2014-12-03 07:35:45 +0100

    Edited templates sheet6

---
+++ sheet6/quadratur2_template.py
@@ -0,0 +1,19 @@
+class Quadrature:
+    # TODO-1 Define this class.
+    # It should implement the following methods:
+    # __init__(self, a, b, n)
+    # integrate(self, f)
+    pass
+
+class Midpoint:
+    # TODO-2 Let this class inherit from the class Quadrature.
+    # It should inherit the __init__ and integrate methods from Quadrature (You don't have to define them again).
+    # It should define the construct method by itself.
+    pass
+
+class Trapezoidal:
+    # TODO-3 Do the same as in TODO-2 for class Trapezoidal. 
+    pass
+
+class MonteCarlo:
+    # TODO-4 This new class should be really easy to implement now.
